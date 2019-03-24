fe81d5ba79c9c5465a4cc257a1ed06865b7c10e3
Tom Hunkapiller <furydevoid@gmail.com>
2019-03-24 05:36:24 -0500

    add old server script

---
+++ bash/progdisc-users.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# a little cgi script for generating a page of all the users on the linux server
+
+ONLINEUSERS="$(who | cut -d' ' -f1) "
+USERS="$(members progdisc-dev | tr ' ' "\n" | sort | tr "\n" ' ')"
+
+OUT=''
+for user_ in $USERS; do
+    OUT="$OUT<a href=\"/~$user_\""
+    if [ -z  "${ONLINEUSERS##*$user_*}" ]; then
+        OUT="$OUT class=\"online\""
+    fi
+    OUT="$OUT>~$user_</a><br>\n"
+done
+
+echo 'HTTP/1.0 200 OK'
+echo "Content-type:text/html\r\n"
+echo '<!doctype html><html><head><title>Programming Discussions - User List</title>'
+echo '<link rel="stylesheet" href="scripts.css"></head><body id="user-list">'
+echo "<main><h1>Server Users</h1>$OUT</main></body></html>"
